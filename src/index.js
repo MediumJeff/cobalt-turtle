7e7fc76ad1a53c59e3ec92979370eb7515cc4c4a
Jeff McCracken <jjmccrac@gmail.com>
2023-04-24 09:46:42 -0700

    Edited fontawesome usage

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
